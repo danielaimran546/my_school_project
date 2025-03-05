40fcc752f7b2bf3deefb8c3d42e9663a6b70b3aa
danielaimran546 <danielaimran546@gmail.com>
2025-03-05 22:40:06 +0000

    Add 1.c

---
+++ src/1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int age = 25;
+    char name[] = "John Doe";
+    float gpa = 3.8;
+    printf("Your name is %s, you are %d years old and your GPA is %.1f", name, age, gpa);
+    return 0;
+}
