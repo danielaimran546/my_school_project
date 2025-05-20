84845a3424d59a55d61b61f6bc4bf35ed0b0ecc0
danielaimran546 <danielaimran546@gmail.com>
2025-05-20 08:26:44 +0000

    Add 67.c

---
+++ src/67.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main() {
+    int i;
+
+    printf("Hello, World!\n");
+
+    for(i = 0; i < 5; i++) {
+        printf("*");
+    }
+
+    printf("\n");
+
+    return 0;
+}
